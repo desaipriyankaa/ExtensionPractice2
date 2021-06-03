d64420ac8a099e6e351e49785de5f87afe0e2e64
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-06-03 10:25:34 +0530

    Add project files.

---
+++ ExtensionPractice2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExtensionPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string demo = "This is a demo";
+            demo.PrintToConsole();
+
+            SimpleLogger loggers = new SimpleLogger();
+            loggers.LogError("This is an error !");
+            loggers.log("Test Error","Errorss");
+        }
+    }
+
+    public static class ExtensionLogger
+    {
+        public static void LogError(this SimpleLogger logger, string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            logger.log(message,"Error");
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+    public class SimpleLogger
+    {
+        public void log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+        public void log(string message, string messageType)
+        {
+            log($"{messageType} : {message}");
+        }
+    }
+}
---
+++ ExtensionPractice2/extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionPractice2
+{
+    public static class extensions
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
